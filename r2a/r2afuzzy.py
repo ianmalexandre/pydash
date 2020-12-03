100a8ab2a68838ebd8ff5d1cd18c04f06273180e
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-12-03 12:55:17 -0300

    Get computed fuzzy using delta time of last two playback segment time

--- r2a/r2afuzzy.py
+++ r2a/r2afuzzy.py
@@ -20,35 +20,57 @@ from r2a.utils.fuzzyfication import fuzzyficationBufferingTime as fuzzyfy
 from r2a.utils.createRules import createRules
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
+from base.whiteboard import Whiteboard
 
-class IR2AFuzzy(IR2A):
+class R2AFuzzy(IR2A):
 
     def __init__(self, id):
+        IR2A.__init__(self, id)
+        self.parsed_mpd = ''
+        self.qi = []
         (self.bufferTime, self.diffBufferTime, self.output) = fuzzyfy()
         self.rules = createRules(self.bufferTime, self.diffBufferTime, self.output)
         self.quality_ctrl = ctrl.ControlSystem(self.rules)
+        self.request_time = time.perf_counter()
 
-    def createSim(self):
+    def createSim(self, bufferTime, diffBufferTime):
         qi = ctrl.ControlSystemSimulation(self.quality_ctrl)
-        qi.input['bufferTime'] = 25.0
-        qi.input['diffBufferTime'] = 0
+        qi.input['bufferTime'] = bufferTime
+        qi.input['diffBufferTime'] = diffBufferTime
 
         qi.compute()
 
-        print(qi.output['output'])
-        self.output.view(sim=qi)
+        return qi.output['output']
+
+        # print(qi.output['output'])
+        # self.output.view(sim=qi)
 
     def handle_xml_request(self, msg):
-        pass
+        self.send_down(msg)
 
     def handle_xml_response(self, msg):
-        pass
+        self.parsed_mpd = parse_mpd(msg.get_payload())
+        self.qi = self.parsed_mpd.get_qi()
+
+        self.send_up(msg)
 
     def handle_segment_size_request(self, msg):
-        pass
+        time_list = self.whiteboard.get_playback_segment_size_time_at_buffer()
+        if len(time_list) < 1:
+            time_list = (35, 1)
+        elif len(time_list) == 1:
+            time_list = (35, time_list[0])
+        time_segment = time_list[-1]
+        delta_time = time_segment - time_list[-2]
+
+        compute_fuzzy = self.createSim(time_segment, delta_time)
+        print(f'Resultado do computing {compute_fuzzy}')
+
+        msg.add_quality_id(self.qi[10])
+        self.send_down(msg)
 
     def handle_segment_size_response(self, msg):
-        pass
+        self.send_up(msg)
 
     def initialize(self):
         pass
